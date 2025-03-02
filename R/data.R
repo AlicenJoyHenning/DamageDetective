30a093f0948eaf03366ab01bf4665b5a4338f7c6
Alicen Henning <129797527+AlicenJoyHenning@users.noreply.github.com>
2025-03-02 13:49:57 +0200

    ►  Create DamageDetective skeleton

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Test Counts
+#'
+#' This dataset contains a sparse matrix for testing purposes.
+#'
+#' @format A sparse matrix with 10 009 features and 500 cells.
+#' \describe{
+#'   \item{features}{Numeric. The number of features (genes) in the dataset.}
+#'   \item{cells}{Numeric. The number of cells in the dataset.}
+#' }
+#' @source Generated for testing purposes
+#' @examples
+#' \donttest{
+#' # Load an example count matrix
+#'  data("test_counts", package = "DamageDetective")
+#'
+#' # Simulate 10 % with the se
+#'
+"test_counts"
